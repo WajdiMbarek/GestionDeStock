6331c23bb8ab833b2a953bebb41c7bb81d6e8666
WajdiMbarek <wajdi.mbarek.ing@gmail.com>
2022-01-23 01:07:08 +0100

    Configure Interceptor to intercept sql statement

--- src/main/java/com/wajdi/GestionDeStock/interceptor/Interceptor.java
+++ src/main/java/com/wajdi/GestionDeStock/interceptor/Interceptor.java
@@ -1,6 +1,23 @@
 package com.wajdi.GestionDeStock.interceptor;
 
 import org.hibernate.EmptyInterceptor;
+import org.springframework.util.StringUtils;
+
+import java.util.Locale;
 
 public class Interceptor extends EmptyInterceptor {
+
+    @Override
+    public  String onPrepareStatement(String sql){
+        if(StringUtils.hasLength(sql) && sql.toLowerCase().contains("select")){
+            if ( sql.contains("where")){
+                sql = sql+ " and idEntreprise = 1";
+            }else{
+                sql = sql+ " where  idEntreprise = 1";
+            }
+
+        }
+        return super.onPrepareStatement(sql);
+    }
+
 }
